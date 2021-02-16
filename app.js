432833b596c99a98a018afee0fbb8f4b2a117862
toxigeno <melissaaraya@yahoo.com>
2021-02-16 23:41:59 +0000

    listo

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the','our'];
+let adj = ['great','big' ];
+let noun = ['jogger','racoon'];
+let end = ['.us', '.com', '.net', '.io']
+
+
+let random = Math.floor(Math.random()*pronoun.length);
+let random1 = Math.floor(Math.random()*adj.length);
+let random2 = Math.floor(Math.random()*noun.length);
+let random3 = Math.floor(Math.random()*end.length);
+
+generator = pronoun[random]+adj[random1]+noun[random2]+end[random3];
+
+console.log(generator);
